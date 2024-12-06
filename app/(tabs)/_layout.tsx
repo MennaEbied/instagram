8090fe164b17b121d47c56be19abee7b11d0eb37
MennaEbied <mennahesham253@gmail.com>
2024-12-06 20:10:47 +0200

    Modify bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import Foundation from "@expo/vector-icons/Foundation";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function TabsLayout() {
   return (
@@ -13,6 +14,15 @@ export default function TabsLayout() {
           headerTitle: "For You",
           tabBarIcon: ({ color }) => (
             <Foundation name="home" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          headerTitle: "Search",
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="search1" size={24} color={color} />
           ),
         }}
       />
