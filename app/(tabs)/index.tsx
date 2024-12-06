dd06f5f534b40d9b21e6214dca4277c762a3052b
MennaEbied <mennahesham253@gmail.com>
2024-12-06 21:11:22 +0200

    Responsive feeds

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,10 +5,11 @@ import { FlatList } from "react-native";
 export default function FeedScreen() {
   return (
     <FlatList
+    className="items-center"
       data={posts}
       renderItem={({ item }) => <PostList post={item} />}
       showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ gap: 12 }}
+      contentContainerStyle={{ gap: 12,maxWidth:512,width:"100%" }}
     />
   );
 }
