2c75fe52679a6c2280eb12cf96d7c88c13610158
MennaEbied <mennahesham253@gmail.com>
2024-12-14 23:47:00 +0200

    Add custom button

---
+++ app/components/customButton.tsx
@@ -0,0 +1,12 @@
+import { Pressable, Text } from "react-native";
+type ButtonProps = {
+  title: string;
+  onPress?: () => void;
+};
+export default function CustomButton({ title, onPress }: ButtonProps) {
+  return (
+    <Pressable className="bg-blue-500 w-full p-3  items-center rounded-2xl" onPress={onPress}>
+      <Text className="text-white font-semibold">{title}</Text>
+    </Pressable>
+  );
+}
