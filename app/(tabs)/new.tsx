63c3b32e29987812165b5beaa88c2f0f2d31c40d
MennaEbied <mennahesham253@gmail.com>
2024-12-14 23:53:02 +0200

    Add custom button to new post page

--- app/(tabs)/new.tsx
+++ app/(tabs)/new.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useState, useEffect } from "react";
+import CustomButton from "../components/customButton";
 
 export default function NewScreen() {
   const [caption, setCaption] = useState("");
@@ -39,25 +40,22 @@ export default function NewScreen() {
         source={{
           uri: imageUri,
         }}
-        className="w-52 aspect-[3/4] rounded-xl bg-slate-500"
+        className="w-52 aspect-[3/4] rounded-xl bg-gray-200"
       />
       <Pressable
         hitSlop={20}
         onPress={handleChooseImage}
-        className="bg-blue-500 w-40 h-15 items-center mt-5 rounded-md"
       >
-        <Text className="text-white m-5 font-semibold">Change</Text>
+        <Text className="text-blue-500 m-5 font-semibold">Change</Text>
       </Pressable>
       <TextInput
         placeholder="What's on your mind?"
-        className="bg-neutral-200 w-full p-4 rounded-sm mt-5"
+        className="bg-neutral-200 w-full p-4 rounded-sm mt-5 "
         value={caption}
-        onChangeText={setCaption}
+        onChangeText={(newValue) => setCaption(newValue)}
       />
       <View className="mt-auto w-full">
-        <Pressable className="bg-blue-500 w-full p-3 h-18 items-center rounded-md">
-          <Text className="text-white font-semibold">Share</Text>
-        </Pressable>
+        <CustomButton title="Share" />
       </View>
     </View>
   );
