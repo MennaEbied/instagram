726103678c5e1788c857ad49b412d8fe8693d356
MennaEbied <mennahesham253@gmail.com>
2024-12-14 23:49:47 +0200

    Add profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,71 @@
-import { Text } from "react-native"
-export default function Profile(){
-    return(
-        <Text>profile</Text>
-    )
+import {
+  Text,
+  View,
+  Image,
+  Platform,
+  Pressable,
+  TextInput,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import CustomButton from "../components/customButton";
+
+export default function Profile() {
+  const [imageUri, setImageUri] = useState<string>();
+  const[username,SetUsername]=useState("")
+  const[email,setEmail]=useState("")
+  const handleChooseImage = async () => {
+    if (Platform.OS === "web") {
+      return;
+    }
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setImageUri(result.assets[0].uri);
+    }
+  };
+  return (
+    <View className="flex-1 items-center p-3">
+      <Image
+        source={{ uri: imageUri }}
+        className="w-40 h-40 rounded-full bg-gray-200 mt-10 aspect-square"
+      />
+      <Pressable
+        hitSlop={20}
+        onPress={handleChooseImage}
+        className="  items-center mt-5"
+      >
+        <Text className="text-blue-500 m-5 font-semibold text-lg">Change</Text>
+      </Pressable>
+      <TextInput
+        placeholder="Type your email"
+        keyboardType="email-address"
+        returnKeyType="done"
+        className="rounded-2xl w-96 p-3 bg-gray-200 mb-5 text-lg"
+        value={email}
+        onChangeText={setEmail}
+      />
+       <TextInput
+        placeholder="Type your username"
+        returnKeyType="done"
+        className="rounded-2xl w-96 p-3 bg-gray-200 text-lg"
+        value={username} onChangeText={SetUsername}
+
+      />
+     {/*  <Pressable className="bg-blue-500 p-5 items-center mt-8 w-4/5 rounded-lg h-13">
+        <Text className="text-white text-lg">Update</Text>
+      </Pressable>
+      <Pressable className="bg-blue-500 p-5 items-center m-5 w-4/5 rounded-lg h-13">
+        <Text className="text-white text-lg">Sign out</Text>
+      </Pressable> */}
+      <View className="gap-3 mt-auto w-full">
+      <CustomButton title="Update profile" />
+      <CustomButton title="Sign out"/>
+      </View>
+    </View>
+  );
 }
