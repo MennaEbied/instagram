3fdb5a17cede1530b59214a23a79ced321d3da8f
MennaEbied <mennahesham253@gmail.com>
2025-03-24 03:44:13 +0200

    Upload images to cloudinary

--- app/(tabs)/new.tsx
+++ app/(tabs)/new.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {
   Text,
   View,
@@ -9,20 +10,20 @@ import {
 import * as ImagePicker from "expo-image-picker";
 import { useState, useEffect } from "react";
 import CustomButton from "../components/customButton";
+import {  uploadImage } from "../lib/cloudinary";
 
 export default function NewScreen() {
   const [caption, setCaption] = useState("");
-  const [imageUri, setImageUri] = useState<string>();
+  const [image, setImage] = useState<string>();
   useEffect(() => {
-    if (!imageUri) {
+    if (!image) {
       handleChooseImage();
     }
-  }, [imageUri]);
+  }, [image]);
   const handleChooseImage = async () => {
     if (Platform.OS === "web") {
       return;
     }
-
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
       allowsEditing: true,
@@ -31,21 +32,26 @@ export default function NewScreen() {
     });
 
     if (!result.canceled) {
-      setImageUri(result.assets[0].uri);
+      setImage(result.assets[0].uri);
     }
+  };
+
+  const createPost = async () => {
+    if (!image) {
+      return;
+    }
+    const response = await uploadImage(image);
+    console.log("image id", response?.public_id);
   };
   return (
     <View className=" flex-1 items-center p-4">
       <Image
         source={{
-          uri: imageUri,
+          uri: image,
         }}
         className="w-52 aspect-[3/4] rounded-xl bg-gray-200"
       />
-      <Pressable
-        hitSlop={20}
-        onPress={handleChooseImage}
-      >
+      <Pressable hitSlop={20} onPress={handleChooseImage}>
         <Text className="text-blue-500 m-5 font-semibold">Change</Text>
       </Pressable>
       <TextInput
@@ -55,7 +61,7 @@ export default function NewScreen() {
         onChangeText={(newValue) => setCaption(newValue)}
       />
       <View className="mt-auto w-full">
-        <CustomButton title="Share" />
+        <CustomButton title="Share" onPress={createPost} />
       </View>
     </View>
   );
